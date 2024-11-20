dcc34b74aeb29b8e54d86ea7a93bfe93896d1fab
Sam Oreste <samoreste50@gmail.com>
2024-11-20 22:39:52 +0800

    Pseudocode

---
+++ personal_info_input/personal_info_input.py
@@ -0,0 +1,5 @@
+# Initiate a list for personal data
+# Types of data(Full name, student id number, phone number, address(city), birthdate(mm/dd/yyyy))
+# Define open/append txt file
+# Define proper input
+# Define ask user for another input
