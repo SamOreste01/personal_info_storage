2bc798a16425c7f1e860e06139db0e4cea563930
Sam Oreste <samoreste50@gmail.com>
2024-11-20 23:08:15 +0800

    Open txt file and append the data inputs

--- personal_info_input/personal_info_input.py
+++ personal_info_input/personal_info_input.py
@@ -9,8 +9,26 @@ info = {}
 full_name = str(input("Enter Full Name: "))
 id_number = input("Enter your Student ID Number: ")
 phone_num = input("Enter Phone Number: ")
-address = input("Enter Address(City)")
-birthdate = input("Enter Birthdate(mm/dd/yyyy)")
+address = input("Enter Address(City): ")
+birthdate = input("Enter Birthdate(mm/dd/yyyy): ")
+
+def open_txt_file():
+    with open('personal_data', 'a') as file_handle:
+        file_handle.write(full_name)
+        file_handle.write(id_number)
+        file_handle.write(phone_num)
+        file_handle.write(address)
+        file_handle.write(birthdate)
+
+
+def valid_input():
+    pass
+
+def ask_input():
+    pass
+
+
+
 
 
 
