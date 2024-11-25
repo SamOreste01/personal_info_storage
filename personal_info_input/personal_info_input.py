8860bf096ae3303b8002ad3b89de20fa757dc5ac
Sam Oreste <samoreste50@gmail.com>
2024-11-25 18:37:27 +0800

    define main, made standards for phone and id number

--- personal_info_input/personal_info_input.py
+++ personal_info_input/personal_info_input.py
@@ -6,18 +6,42 @@
 
 info = []
 phone_digits = 11
+id_number_digits = 15
 
-full_name = str(input("Enter Full Name: "))
-id_number = input("Enter your Student ID Number (ex. 2024-01234-MN-0): ")
-phone_num = input("Enter Phone Number: ") 
-if len(phone_num) != phone_digits:
-    print("You're phone number must be 11 digits")
-address = str(input("Enter your Address(City): "))
-birthdate = input("Enter your Birthdate(mm/dd/yyyy): ")
+def main():
+    while True:
+        try:
+            full_name = str(input("Enter Full Name: "))
+        except ValueError:
+            ("Invalid Input!")
+        
+        while True:
+            id_number = input("Enter your Student ID Number (ex. 2024-01234-MN-0): ")
+            if len(id_number) == id_number_digits:
+                break
+            else: 
+                print("You're id number must have 15 characters")
 
-user_input = [full_name, id_number, phone_num, address, birthdate]
+        while True:
+            phone_num = input("Enter Phone Number: ") 
+            if len(phone_num) != phone_digits:
+                break
+            else:
+                print("You're phone number must be 11 digits")
+        
+        try:
+            address = str(input("Enter your Address(City): "))
+        except ValueError:
+            ("Invalid Input!")
 
-info.append(user_input)
+        try:
+            birthdate = input("Enter your Birthdate(mm/dd/yyyy): ")
+        except ValueError:
+            ("Invalid Input!")
+
+        user_input = [full_name, id_number, phone_num, address, birthdate]
+
+        info.append(user_input)
 
 def open_txt_file():
     pass
@@ -25,6 +49,7 @@ def open_txt_file():
 def ask_input():
     pass
 
+main()
 print(info)
 
 
