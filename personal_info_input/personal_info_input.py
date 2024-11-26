b64cc6c690c9d99db62a46db1720481803722a84
Sam Oreste <samoreste50@gmail.com>
2024-11-26 12:53:18 +0800

    added type of data: sex and marital status

--- personal_info_input/personal_info_input.py
+++ personal_info_input/personal_info_input.py
@@ -1,5 +1,5 @@
 # Initiate a list for personal data
-# Types of data(Full name, student id number, phone number, address(city), birthdate(mm/dd/yyyy))
+# Types of data(Full name, student id number, phone number, address(city), Sex, birthdate(mm/dd/yyyy), Marital status)
 # Define open/append txt file
 # Define main that includes:
 # Input standards, ask user for another input
@@ -12,50 +12,66 @@ birthdate_charac = 10
 def main():
     while True:
         try:
-            full_name = str(input("Enter Full Name: "))
+            full_name = str(input("Enter Full Name: ")) # Asks user for their full name
         except Exception:
             ("Invalid Input!")
         
         while True:
-            id_number = input("Enter your Student ID Number (ex. 2024-01234-MN-0): ")
+            id_number = input("Enter your Student ID Number (ex. 2024-01234-MN-0): ") # Asks user for their id number and restricts them to obey the given format
             if len(id_number) == id_number_digits:
                 break
             else: 
-                print("You're id number must have 15 characters, please follow the format given")
+                print("Your id number must have 15 characters, please follow the format given")
 
         while True:
-            phone_num = input("Enter Phone Number: ") 
+            phone_num = input("Enter Phone Number: ")  # Asks user for their phone number and restricts them to 11 digits format
             if len(phone_num) == phone_digits:
                 break
             else:
-                print("You're phone number must be 11 digits")
+                print("Your phone number must be 11 digits")
         
         try:
-            address = str(input("Enter your Address(City): "))
+            address = str(input("Enter your Address(City): ")) # Asks user for address
         except Exception:
             ("Invalid Input!")
 
         while True:
-            birthdate = input("Enter your Birthdate(mm/dd/yyyy): ")
+            print("M - male, F - Female, NA - Prefer not to say") #Asks user for sex
+            sex = input("Enter your sex (M/F/NA): ")
+            if sex == "M" or "F" or "NA":
+                break
+            else:
+                print("Choose from M/F/NA")
+
+        while True:
+            birthdate = input("Enter your Birthdate(mm/dd/yyyy): ") # Asks user for their birthdate and restricts them to obey the given format
             if len(birthdate) == birthdate_charac:
                 break
             else: 
-                print("You're birthdate must have 10 characters, please follow the format given")
+                print("Your birthdate must have 10 characters, please follow the format given")
 
-        choice = (input("Do you want to enter another entry? (Y/N): "))
+        while True:
+            print("SI - Single, M - Married, W - Widowed, SP - Seperated") 
+            marital_stat = input("Enter your Marital Status (SI/M/W/SP): ") # Asks user for marital status
+            if marital_stat == "SI" or "M" or "W" or "SP":
+                break
+            else:
+                print("Choose from SI/M/W/SP")
+
+        user_input = f"{full_name}, {id_number}, {phone_num}, {address}, {birthdate}\n" 
+        info.append(user_input)
+
+        choice = (input("Do you want to enter another entry? (Y/N): ")) # Asks user if they want to input another or leave
         if choice == "Y":
             continue
         else:
+            print("Now leaving the program :D")
             break
-
-    user_input = f"{full_name}, {id_number}, {phone_num}, {address}, {birthdate}\n"
-    info.append(user_input)
 
 def file_handle():
     with open("personal_info.txt", "a") as file:
             file.writelines(info)
     print("All information was successfully saved to 'personal_info.txt'")
-
 
 main()
 file_handle()
