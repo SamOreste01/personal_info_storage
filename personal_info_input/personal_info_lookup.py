7764751c535e43fabcc6c752dcb10af9f06156d9
Sam Oreste <samoreste50@gmail.com>
2024-11-29 18:44:25 +0800

    Revised minor errors

--- personal_info_input/personal_info_lookup.py
+++ personal_info_input/personal_info_lookup.py
@@ -4,22 +4,26 @@
 # Search full name on txt file
 # Ask user if they want to search another
 
-entry = []
-
 def name_lookup():
     while True:
         full_name = input("Enter full name of the person you're looking for: ") 
         if full_name == "":
             print("Please Enter a Name")
+            continue
                  
         try:
             with open("personal_info.txt", "r") as file:
+                entry = []
                 for line in file:
                     if full_name in line:
-                        entry.append(line)
+                        entry.append(line.strip())
+                           
+                if not entry:
+                    print(f"{full_name}'s Information was not found")
 
-                    else:
-                        print(f"{full_name}'s Information was not found")
+                else:
+                    print("Format: [Full Name, Id Number, Phone Number, City Address, Sex, Birthdate, Marital Status]")
+                    print(f"{full_name}'s Information was found: {entry}")
         
         except FileNotFoundError:
             print("File was not found")
@@ -34,4 +38,3 @@ def name_lookup():
             break
         
 name_lookup()
-print(entry)
