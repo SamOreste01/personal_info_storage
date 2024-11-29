85efa7237dd5d521b525910202375aefdbc0a51c
Sam Oreste <samoreste50@gmail.com>
2024-11-29 16:18:25 +0800

    Ask user for full name, open txt file, search name in txt file

--- personal_info_input/personal_info_lookup.py
+++ personal_info_input/personal_info_lookup.py
@@ -1,4 +1,21 @@
+# Initiate an array
 # Ask user for full name
 # Open txt file
 # Search full name on txt file
 # Ask user if they want to search another
+
+entry = []
+
+def name_lookup():
+    full_name = input("Enter full name of the person you're looking for: ") 
+
+    with open("personal_info.txt", "r") as file:
+        for line in file:
+            if full_name in line:
+                entry.append(line)
+
+name_lookup()
+print(entry)
+
+
+
